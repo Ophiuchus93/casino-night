8924acadecfda8dd0d677367d09e4413a64ee7cb
Scott (DC)  Christensen <schristensen10@gmail.com>
2019-08-16 15:47:04 -0600

    initial commit

---
+++ casino_night.rb
@@ -0,0 +1,11 @@
+# Menu
+# Ask for user name and age
+# if not 21 then they get kicked off
+# ask how much money they have (wallet)
+# use ASCii images 
+# create menu with the different games
+
+require "colorize"
+require "pry"
+require "colorized_string"
+
