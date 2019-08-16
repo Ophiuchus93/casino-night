6e69858be6a27eec5b80093e5aa79da33c361b43
Ryan Pickett <piratematic@gmail.com>
2019-08-16 16:41:14 -0600

    Clorized and added continue option

--- spin.rb
+++ spin.rb
@@ -14,7 +14,28 @@ class Spin
       puts "You Win"
     else
       puts "You lose D:"
+      sleep(2)
+      puts "Would you like to play again?"
+      puts "1) Yes"
+      puts "2) No"
+      choice = gets.to_i
+      case choice
+      when 1
+        puts `clear`
+    
+        puts "-----------    --------   ----    ---- ----         ------------ ------------ ------------ ------------ ".colorize(:light_magenta)
+        puts "***********   **********  ****    **** ****         ************ ************ ************ ************ ".colorize(:magenta)
+        puts "----    ---  ----    ---- ----    ---- ----         ----         ------------ ------------ ----         ".colorize(:light_blue)
+        puts "*********    ***      *** ****    **** ****         ************     ****         ****     ************ ".colorize(:blue)
+        puts "---------    ---      --- ----    ---- ----         ------------     ----         ----     ------------ ".colorize(:light_cyan)
+        puts "****  ****   ****    **** ************ ************ ****             ****         ****     ****         ".colorize(:cyan)
+        puts "----   ----   ----------  ------------ ------------ ------------     ----         ----     ------------ ".colorize(:light_green)
+        puts "****    ****   ********   ************ ************ ************     ****         ****     ************ ".colorize(:green)
 
+        Spin.new
+      when 2
+        exit #Main Menu
+      end
     end
   end
 end
