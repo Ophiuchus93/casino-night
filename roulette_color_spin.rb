e166d8482b3c9306b1cc7453b2fa66bef5e08bc9
Ryan Pickett <piratematic@gmail.com>
2019-08-18 15:52:55 -0600

    Added animations to roulette and beting functions

--- roulette_color_spin.rb
+++ roulette_color_spin.rb
@@ -43,17 +43,21 @@ class ColorSpin
   end
 
   def pick_color
+    puts "Place your Bet"
+    print ">$"
+    @user_bet = gets.to_i
     puts "Pick a color to bet on: Black, Red, or Green"
     print "> "
     answer = gets.strip.downcase
     if answer == @color_selection.sample 
       puts "You win" 
-    else
-      puts "You lose"
+      money_difference = @wallet.to_i + @user_bet.to_i
+      @wallet = money_difference
       sleep(2)
       puts "Would you like to play again?"
       puts "1) Yes"
       puts "2) No"
+      print "> "
       choice = gets.to_i
       case choice
       when 1
@@ -70,7 +74,34 @@ class ColorSpin
 
         ColorSpin.new
       when 2
-        exit #Main Menu
+        Roulette.new
+      end
+    else
+      puts "You lose"
+      money_difference = @wallet.to_i + @user_bet.to_i
+      @wallet = money_difference
+      sleep(2)
+      puts "Would you like to play again?"
+      puts "1) Yes"
+      puts "2) No"
+      print "> "
+      choice = gets.to_i
+      case choice
+      when 1
+        puts `clear`
+    
+        puts "-----------    --------   ----    ---- ----         ------------ ------------ ------------ ------------ ".colorize(:light_magenta)
+        puts "***********   **********  ****    **** ****         ************ ************ ************ ************ ".colorize(:magenta)
+        puts "----    ---  ----    ---- ----    ---- ----         ----         ------------ ------------ ----         ".colorize(:light_blue)
+        puts "*********    ***      *** ****    **** ****         ************     ****         ****     ************ ".colorize(:blue)
+        puts "---------    ---      --- ----    ---- ----         ------------     ----         ----     ------------ ".colorize(:light_cyan)
+        puts "****  ****   ****    **** ************ ************ ****             ****         ****     ****         ".colorize(:cyan)
+        puts "----   ----   ----------  ------------ ------------ ------------     ----         ----     ------------ ".colorize(:light_green)
+        puts "****    ****   ********   ************ ************ ************     ****         ****     ************ ".colorize(:green)
+
+        ColorSpin.new
+      when 2
+        Roulette.new
       end
     end
   end
--- spin.rb
+++ spin.rb
@@ -6,18 +6,22 @@ class NumberSpin
   end
   
   def spin_wheel
+    puts "Place your Bet"
+    print ">$"
+    @user_bet = gets.to_i 
     puts "Pick a number between 0-36"
     pick = gets.to_i
 
     spin = rand(0..36)
     if pick == spin
       puts "You Win"
-    else
-      puts "You lose D:"
+      money_difference = @wallet.to_i + @user_bet.to_i
+      @wallet = money_difference
       sleep(2)
       puts "Would you like to play again?"
       puts "1) Yes"
       puts "2) No"
+      print "> "
       choice = gets.to_i
       case choice
       when 1
@@ -34,7 +38,34 @@ class NumberSpin
 
         NumberSpin.new
       when 2
-        exit #Main Menu
+        Roulette.new
+      end
+    else
+      puts "You lose D:"
+      money_difference = @wallet.to_i - @user_bet.to_i
+      @wallet = money_difference
+      sleep(2)
+      puts "Would you like to play again?"
+      puts "1) Yes"
+      puts "2) No"
+      print "> "
+      choice = gets.to_i
+      case choice
+      when 1
+        puts `clear`
+    
+        puts "-----------    --------   ----    ---- ----         ------------ ------------ ------------ ------------ ".colorize(:light_magenta)
+        puts "***********   **********  ****    **** ****         ************ ************ ************ ************ ".colorize(:magenta)
+        puts "----    ---  ----    ---- ----    ---- ----         ----         ------------ ------------ ----         ".colorize(:light_blue)
+        puts "*********    ***      *** ****    **** ****         ************     ****         ****     ************ ".colorize(:blue)
+        puts "---------    ---      --- ----    ---- ----         ------------     ----         ----     ------------ ".colorize(:light_cyan)
+        puts "****  ****   ****    **** ************ ************ ****             ****         ****     ****         ".colorize(:cyan)
+        puts "----   ----   ----------  ------------ ------------ ------------     ----         ----     ------------ ".colorize(:light_green)
+        puts "****    ****   ********   ************ ************ ************     ****         ****     ************ ".colorize(:green)
+
+        NumberSpin.new
+      when 2
+        Roulete.new
       end
     end
   end
